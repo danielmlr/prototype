a5d8868136b3b223bc357c5b31901d6e6086094c
danielmlr <daniel.mu01@gmail.com>
2022-09-07 16:38:42 +0200

    .

--- apps/sveltekit/src/routes/+page.svelte
+++ apps/sveltekit/src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+	import { ITERM_PROFILE } from '$env/static/private';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
